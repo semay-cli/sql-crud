c34fd4681564b8746783d4ff1ab048387c252cd7
Beimnet B Degefu <beimnet.degefu.com>
2025-12-10 21:52:16 +0300

    with service in between controller and repo

--- generate/login.go
+++ generate/login.go
@@ -4,8 +4,10 @@ import "github.com/semay-cli/sql-crud/stemplates"
 
 func GenerateSSOLogin(data stemplates.ProjectSetting) {
 	tmplService := stemplates.LoadTemplate("loginService")
+	tmplRepository := stemplates.LoadTemplate("loginRepository")
 	tmplController := stemplates.LoadTemplate("loginControl")
 
-	stemplates.WriteTemplateToFileSetting("repository/login_repo.go", tmplService, data)
+	stemplates.WriteTemplateToFileSetting("repository/login_repo.go", tmplRepository, data)
+	stemplates.WriteTemplateToFileSetting("services/login_svc.go", tmplService, data)
 	stemplates.WriteTemplateToFileSetting("controllers/login_controller.go", tmplController, data)
 }
